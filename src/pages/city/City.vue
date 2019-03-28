69724631bfae6cde97ec65fd12efd20930951bb5
yang <Cadenyang5255@163.com>
2019-03-28 11:14:04 +0800

    增加城市列表功能

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,47 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list :hot="hotCities" :cities="allCities"></city-list>
+        <city-alphabet :cities="allCities"></city-alphabet>
     </div>
 </template>
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
 
 export default {
     name: 'City',
     components: {
         CityHeader,
-        CitySearch
+        CitySearch,
+        CityList,
+        CityAlphabet
+    },
+    data () {
+        return {
+            hotCities: [],
+            allCities: {}
+        }
+    },
+    mounted () {
+        this.getCityInfo()
+    },
+    methods: {
+        getCityInfo () {
+            axios.get('/api/city.json').then(response => {
+                const res = response.data
+                if(res.data && res.success == true){
+                    this.hotCities = res.data.hotCities
+                    this.allCities = res.data.cities
+                    // this.iconList = res.data.iconList
+                    // this.recommendList = res.data.recommendList
+                    // this.weekendList = res.data.weekendList
+                }
+            })
+        }
     }
 }
 </script>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom">您的位置</div>
+                    <div class="button-list">
+                        <div class="button-wipper">
+                            <div class="button">北京</div>
+                        </div>
+                    </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">热门城市</div>
+                    <div class="button-list">
+                        <div class="button-wipper" v-for="item of hot" :key="item.id">
+                            <div class="button">{{ item.name }}</div>
+                        </div>
+                    </div>
+            </div>
+            <div class="area" v-for="(item, key) of cities" :key="key">
+                <div class="title border-topbottom">{{ key }}</div>
+                    <div class="item-list">
+                        <div 
+                            class="item border-bottom" 
+                            v-for="innerItem of item" 
+                            :key="innerItem.id"
+                        >{{ innerItem.name}}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import Bscroll from 'better-scroll'
+
+export default {
+    name: 'CityList',
+    props: {
+        hot: Array,
+        cities: Object
+    },
+    mounted () {
+        this.scroll = new Bscroll(this.$refs.wrapper)
+    }
+}
+</script>
+<style lang="stylus" scoped>
+    @import '~@/assets/css/varibles.styl'
+    .border-topbottom {
+        &:before {
+            border-color: #ccc
+        }
+        &:after {
+            border-color: #ccc
+        }
+    }
+    .border-bottom {
+        &:before {
+            border-color: #ccc
+        }
+    }
+    .list {
+        overflow: hidden
+        position: absolute
+        top: 1.58rem
+        left: 0
+        right: 0
+        bottom: 0
+    }
+    .title {
+        line-height: .6rem 
+        background: #eee
+        padding-left: .2rem
+        color: #666
+        font-size: .26rem
+    }
+    .button-list {
+        padding: .1rem .6rem .1rem .1rem
+        overflow: hidden
+    }
+    .button-wipper {
+        float: left
+        width: 33.33%
+    }
+    .button {
+        padding: .1rem 0
+        margin: .1rem
+        text-align: center
+        border: .02rem solid #ccc
+        border-radius: .06rem
+    }
+    .item {
+        line-height: .76rem
+        padding-left: .2rem
+    }
+</style>
+
