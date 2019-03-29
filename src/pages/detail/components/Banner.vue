d65896733990c5e43df6b10fbb346499b17e88bd
yang <Cadenyang5255@163.com>
2019-03-29 11:18:10 +0800

    详情页banner

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="banner">
-        <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1903/93/939f4ea1d9f2f58da3.water.jpg_600x330_d5f6aebe.jpg" />
+        <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" />
+        <div class="banner-info">
+            <div class="banner-title">故宫(AAAAA景区)</div>
+            <div class="banner-number"><span class="iconfont banner-icon">&#xe692;</span>80</div>
+        </div>
     </div>
 </template>
 <script>
@@ -8,8 +12,45 @@ export default {
     name: 'DetailBanner'
 }
 </script>
-<style>
 
+<style lang="stylus" scoped>
+.banner {
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55.3%
+}
+.banner-img {
+    width: 100%
+}
+.banner-info {
+    display: flex
+    position: absolute
+    left: 0
+    right: 0
+    bottom: 0
+    line-height: .6rem
+    color:#fff
+    background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+}
+.banner-title {
+    flex: 1
+    font-size: .35rem
+    padding: 0 .2rem
+}
+.banner-number {
+    height: .35rem
+    line-height: .35rem
+    margin-top: .15rem
+    padding: 0 .3rem
+    border-radius: .2rem
+    background: rgba(0, 0, 0, .8)
+    font-size: .35rem 
+}
+.banner-icon {
+    font-size: .35rem
+    margin-right: .1rem
+}
 </style>
 
 
